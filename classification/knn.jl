4d7b670ae06b8dc0e65b7547a519f414d7ffbb60
Dhruva Ahuja <cooljumpin@gmail.com>
2025-12-22 11:19:06 +0530

    Added k-nearest-neighbors algorithm in classification.

---
+++ classification/knn.jl
@@ -0,0 +1,20 @@
+import DataStructures: counter
+
+cosmic_objects = [
+    ((1, 5), "Dwarf Star"),
+    ((3, 8), "Giant Star"),
+    ((2, 6), "Dwarf Star"),
+]
+
+new_object = (2, 7)
+
+function k_nearest_neighbors(data, query, k, distance_fn) 
+    distances = map(x -> (distance_fn(x[1], query), x[2]), data)
+    sort!(distances, rev=true)
+    c = counter(map(x -> x[2], distances[1:k]))
+    sort!(collect(c), by = x -> x[2], rev=true)[1][1]
+end
+
+euclidian_distance = (x, y) -> [(x1 - y1)^2 for (x1, y1) in zip(x, y)] |> sum |> sqrt
+
+k_nearest_neighbors(cosmic_objects, new_object, 3, euclidian_distance)
